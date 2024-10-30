d8c1cac69ecb40b211f0cfdee62ec762c4284702
college hill <572331+rapka@users.noreply.github.com>
2024-10-30 18:43:54 -0400

    Updates

---
+++ precache-manifest.2743faf5a8ea42c7c7ce7131d17d3184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfb38d5db219a2841831215661801fb",
+    "url": "/state-purity/index.html"
+  },
+  {
+    "revision": "e5fc54d9cb9b55562213",
+    "url": "/state-purity/static/css/main.85d10231.chunk.css"
+  },
+  {
+    "revision": "2748dee9dfc95b704c65",
+    "url": "/state-purity/static/js/2.b11db0cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/state-purity/static/js/2.b11db0cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5fc54d9cb9b55562213",
+    "url": "/state-purity/static/js/main.8fa176b2.chunk.js"
+  },
+  {
+    "revision": "d9630de1f2b7aa778689",
+    "url": "/state-purity/static/js/runtime-main.8adba17f.js"
+  }
+]);
